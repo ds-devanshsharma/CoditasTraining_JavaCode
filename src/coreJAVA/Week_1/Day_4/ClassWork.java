cb1b08e4b467941bffcbd8c01160ebabb30934ed
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-07-22 11:14:46 +0530

    this is commit after day4 ForEachDemo

--- src/coreJAVA/Week_1/Day_4/ClassWork.java
+++ src/coreJAVA/Week_1/Day_4/ClassWork.java
@@ -34,10 +34,34 @@ package coreJAVA.Week_1.Day_4;
                 System.out.println("tum bs pass ho gya bhai");
         }
     }
+    public void for_loopDemo(int num){
+        for(int i =1;i<num ;i++)
+            System.out.println("counter : "+ i);
+
+     }
+    public void whileDemo(int num){
+        while(num>0){
+            System.out.println("counter : " + num);
+            num--;
+        }
+    }
+    public void doWhileDemo(int num){
+       do{ System.out.println("counter : " + num);
+           num--;
+         }while(num<0);
+    }
+    public void forEachDemo(char[] array){
+        for(char each: array)
+            System.out.println(" char "+ each);
+    }
 }
 public class ClassWork{
     public static void main(String[] args) {
         ClassWork_Things classWork_things=new ClassWork_Things();
-        classWork_things.switchDemo(5);
+        //classWork_things.switchDemo(5);
+        //classWork_things.for_loopDemo(10);
+        //classWork_things.whileDemo(10);
+        //classWork_things.doWhileDemo(10);
+        classWork_things.forEachDemo(new char[]{'a','b','c','d'});
     }
 }
