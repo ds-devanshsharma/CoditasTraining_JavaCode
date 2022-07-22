5bd46bedae04f8746e14b2e738775ee3b60b97ef
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-07-22 14:55:15 +0530

    this is commit on last Age Question

--- src/coreJAVA/Week_1/Day_4/Days.java
+++ src/coreJAVA/Week_1/Day_4/Days.java
@@ -1,7 +1,12 @@
 package coreJAVA.Week_1.Day_4;
+
+import java.util.Scanner;
+
 class Days_code{
-    public void dayPrinter(String[] string){
+    public void dayPrinter(String[] string ,String today){
         for (String day: string) {
+            if(day.equalsIgnoreCase(today))
+                continue;
             System.out.println(day);
         }
     }
@@ -12,6 +17,10 @@ public class Days {
                 "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"
         };
         Days_code days_code = new Days_code();
-        days_code.dayPrinter(week);
+//        days_code.dayPrinter(week);
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter today's name : ");
+        String today= scanner.next();
+        days_code.dayPrinter(week,today);
     }
 }
