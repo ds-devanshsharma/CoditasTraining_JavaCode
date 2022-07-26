0147718d9eed222cc2419616e2785c5e64aabbe0
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-07-26 16:24:47 +0530

    this is commit on day 6

---
+++ src/coreJAVA/Week_1/Day_5/ClassWork.java
@@ -0,0 +1,33 @@
+package coreJAVA.Week_1.Day_5;
+
+import javax.xml.namespace.QName;
+
+// Rule 1: never use non-static in static
+// Rule 2: static is called as initializer or load initialzer block
+// Rule 3: within static block you can use static as well as Local variables
+class ClassWork_Code{
+    // showing
+}
+// class Demo{
+//    void ml (Demo demo){
+//        System.out.println("Instance method");
+//    }
+//    static void ml(Demo d) {
+//        System.out.println("Static method");
+//    }
+//}
+
+
+public class ClassWork {
+
+   /* String name= "Batch2" ;
+    static int adhaar=1234;
+
+    public static void accept(){
+            System.out.println("i am from static "+ adhaar + " "+ name);
+            System.out.println("i am from static "+ adhaar );
+    }*/
+    public static void main(String[] args) {
+
+    }
+}
