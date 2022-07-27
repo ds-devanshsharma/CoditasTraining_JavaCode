75c9d631dbbd637db7634b6b23c74f3e9b595dc6
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-07-27 16:29:07 +0530

    this is commit on day 7 on Doing ManagerClass

--- src/coreJAVA/Week_1/Day_7/Ques1.java
+++ src/coreJAVA/Week_1/Day_7/Ques1.java
@@ -1,7 +1,6 @@
 package coreJAVA.Week_1.Day_7;
 
 import java.util.Scanner;
-
 /**
 1. Create class WageEmployee extending Employee class with attributes as hrs (int)and
 rate int and method computeSalary() to calculate the salary.
@@ -20,30 +19,112 @@ class Employee{
         System.out.println("Employee empID : "+ emp_id + "\nEmployee Name : "+name);
     }
 }
+
+/**
+  3. Create Manager class extending Employee class with attributes as fixed salary(int) and
+  incentives(int) and method compute Salary() to calculate the salary of Manager .Print the salary
+  and details of Manager
+ */
+class Manager extends Employee{
+    int fixedSalary;
+    int incentives;
+    Manager(int emp_id, String name,int fixedSalary,int incentives) {
+        super(emp_id, name);
+        this.fixedSalary = fixedSalary;
+        this.incentives = incentives;
+    }
+    public int computeSalary(){
+
+        return fixedSalary+incentives ;
+    }
+}
 class WageEmployee extends Employee{
     int hrs;
     int rate;
-    WageEmployee(int emp_id,String name) {
+    WageEmployee(int emp_id,String name,int hrs,int rate) {
         super(emp_id,name);
-        super.detailsPrinter();
-    }
-    public int computeSalary(int hrs,int rate){
         this.rate=rate;
         this.hrs=hrs;
+        super.detailsPrinter();
+
+    }
+    public int computeSalary(){
+
         return hrs*rate;
     }
 }
+
+/**
+  2. Create SalesPerson class extending WageEmployee with attributes as sales(int) and
+  commission (int).
+  Override the Compute Salary() in Salesperson class and print the salary and details of
+  SalesPerson
+ */
+class SalesPerson extends WageEmployee{
+    int sales;
+    int commission;
+    SalesPerson(int sales, int commission,int hrs,int rate,int emp_id,String name) {
+        super(emp_id,name,hrs,rate);
+        this.commission=commission;
+        this.sales=sales;
+
+    }
+    public int computeSalary(){
+
+        return (hrs*rate)+commission ;
+    }
+}
+
+/**
+ 4. Write a TestEmployee class to print the details of all types of employees (use array[ of
+    Employee class
+ */
+class TestEmployee{
+    Employee[] employees =new Employee[3];
+
+}
+
 public class Ques1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your ID & name : ");
         int id = scanner.nextInt();
         String name = scanner.next();
-        WageEmployee employee1 =new WageEmployee(id,name);
+//        System.out.println("Enter your Timing & rate : ");
+//        int hrs =scanner.nextInt();
+//        int rate=scanner.nextInt();
+//        System.out.println("Enter your sales count & Commisson :  ");
+//        int sales=scanner.nextInt();
+//        int commisson=scanner.nextInt();
+//        SalesPerson employee1 =new SalesPerson(sales,commisson,hrs,rate,id,name);
 
-        System.out.println("Enter your Timing & rate : ");
-        int hrs =scanner.nextInt();
-        int rate=scanner.nextInt();
-        System.out.println("Salary of Employee is : "+employee1.computeSalary(hrs,rate));
+        /*
+        Enter your ID & name :
+        11056
+        Devansh
+        Enter your Timing & rate :
+        8
+        1500
+        Enter your sales count & Commisson :
+        8
+        1500
+        Employee empID : 11056
+        Employee Name : Devansh
+        Salary of Employee is : 13500
+         */
+        System.out.println("HI manager!! Please Enter your fixedSalary & incentives : ");
+        int fixedSalary = scanner.nextInt();
+        int incentives = scanner.nextInt();
+        Manager manager = new Manager(id,name,fixedSalary,incentives);
+        System.out.println("Gross Salary of Employee is : "+manager.computeSalary());
+        /*
+        Enter your ID & name :
+        11056
+        Devansh
+        HI manager!! Please Enter your fixedSalary & incentives :
+        45000
+        250
+        Gross Salary of Employee is : 45250
+         */
     }
 }
