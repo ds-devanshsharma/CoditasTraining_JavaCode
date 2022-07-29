9998b57cccb751578b9d1ea262852d28a998d671
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-07-29 15:20:14 +0530

    Commit on Day 9 Assignment

--- src/coreJAVA/Week_1/Day_9/Ques5.java
+++ src/coreJAVA/Week_1/Day_9/Ques5.java
@@ -3,10 +3,15 @@ package coreJAVA.Week_1.Day_9;
 /**
  * program for Addition of multiple numbers
  */
-abstract class Addition{
-    abstract int additionCalculator(int... numbers);
+ class Addition{
+    int additionCalculator(int number1,int number2) {
+        return number1 + number2;
+    }
+    float additionCalculator(float number1 ,int number2){
+        return number1 + number2;
+    }
 }
-class VarArgAddition extends Addition{
+class VarArgAddition {
     int additionCalculator(int... numbers){
         int sum = 0;
         for(int number : numbers)
@@ -16,9 +21,14 @@ class VarArgAddition extends Addition{
 }
 public class Ques5 {
     public static void main(String[] args) {
-        VarArgAddition addition = new VarArgAddition();
-        System.out.println("Sum : " + addition.additionCalculator(1, 2, 3, 4));
-        System.out.println("Sum : " + addition.additionCalculator(10, 20));
+
+        Addition addition1 = new Addition();
+        System.out.println("This is from Addition class : "+addition1.additionCalculator(10,15));
+        System.out.println("This is from Addition class :"+addition1.additionCalculator(10.5f,15));
+
+        VarArgAddition addition2= new VarArgAddition();
+        System.out.println("Sum from VarArgClass : " + addition2.additionCalculator(1, 2, 3, 4));
+        System.out.println("Sum from VarArgClass : " + addition2.additionCalculator(10, 20));
     }
 }
         /*
