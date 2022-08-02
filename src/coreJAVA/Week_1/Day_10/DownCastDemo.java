21580f4c50df515d0e75a7e62d5229e96a0ed7b2
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-08-03 00:40:09 +0530

    Commit on Day 10 DownCasting Demo

--- src/coreJAVA/Week_1/Day_10/DownCastDemo.java
+++ src/coreJAVA/Week_1/Day_10/DownCastDemo.java
@@ -1,6 +1,23 @@
 package coreJAVA.Week_1.Day_10;
-class DownCast{
+class DownCastParent{
 
 }
-public class DownCastDemo {
+class DownCastChild extends DownCastParent{
+    static void downCast(DownCastParent downCastParent){
+        if(downCastParent instanceof DownCastChild) {
+            DownCastChild downCastChild = (DownCastChild) downCastParent;
+        }
+        System.out.println("DownCastChild downCastChild = (DownCastChild) downCastParent; \nThis is downCasting !!");
+    }
 }
+public class DownCastDemo {
+    public static void main(String[] args) {
+        DownCastParent downCastParent = new DownCastParent();
+        DownCastChild.downCast(downCastParent);
+    }
+}
+/*
+DownCastChild downCastChild = (DownCastChild) downCastParent;
+This is downCasting !!
+
+ */
