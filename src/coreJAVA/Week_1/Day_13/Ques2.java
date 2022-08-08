eddba090f3928b4379b5a4a5d6eb0b240c7993e0
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-08-08 15:39:10 +0530

    Commit on  day13 Ques1 Ques2

---
+++ src/coreJAVA/Week_1/Day_13/Ques2.java
@@ -0,0 +1,25 @@
+package coreJAVA.Week_1.Day_13;
+/**
+ *  String into int
+ */
+public class Ques2{
+    public static void main(String[] args) {
+        String string = "number";
+       try{
+           System.out.println("Before exception !");
+          int number = Integer.parseInt(string);
+       }catch (Exception e){
+           e.printStackTrace();
+       }
+    }
+}
+/*
+Before exception !
+java.lang.NumberFormatException: For input string: "number"
+	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+	at java.base/java.lang.Integer.parseInt(Integer.java:668)
+	at java.base/java.lang.Integer.parseInt(Integer.java:784)
+	at coreJAVA.Week_1.Day_13.Ques3.main(Ques3.java:8)
+
+NumberFormatException is unchecked Exception that occurs whenever number is expected and other dataType is passed.
+ */
