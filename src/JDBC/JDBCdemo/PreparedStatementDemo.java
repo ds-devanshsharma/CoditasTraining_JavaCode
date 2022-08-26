90ae1faaf239b51e5518dae65879818826ad3c0b
devansh-sharma-coditas <devanshsharmads9935@gmail.com>
2022-08-26 16:35:11 +0530

    Commit on JDBC PreparedStatement  Insert  operation

--- src/JDBC/JDBCdemo/PreparedStatementDemo.java
+++ src/JDBC/JDBCdemo/PreparedStatementDemo.java
@@ -1,11 +1,14 @@
 package JDBC.JDBCdemo;
 
 import java.sql.*;
+import java.util.Scanner;
 
 class PreparedStatementcode{
     Connection connection ;
     PreparedStatement preparedStatement;
     ResultSet resultSet;
+    Scanner scanner ;
+    int check;
     void CRUD(){
         try {
             // Driver
@@ -15,7 +18,20 @@ class PreparedStatementcode{
             connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_database ",
                     "root","Coditas@123");
             //statement
-            preparedStatement = connection.prepareStatement("SELECT * FROM JAVA_PEEPS");
+            //preparedStatement = connection.prepareStatement("SELECT * FROM JAVA_PEEPS");
+            preparedStatement = connection.prepareStatement("Insert into JAVA_PEEPS values (?,?,?)");
+            // taking input from USER
+
+            scanner = new Scanner(System.in);
+            System.out.println("Enter ID : ");
+            preparedStatement.setInt(1,scanner.nextInt());
+            System.out.println("Enter Name : ");
+            preparedStatement.setString(2,scanner.next());
+            System.out.println("Enter City : ");
+            preparedStatement.setString(3,scanner.next());
+            //excuteUpdate
+            check=preparedStatement.executeUpdate();
+            if(check>0) System.out.println("Data Inserted  Succesfully !");
             //executing Query
             resultSet =preparedStatement.executeQuery();
             if(resultSet!=null) System.out.println("RECORD FETCHED FROM DATABASE !");
@@ -39,4 +55,10 @@ using preparedStatement
 RECORD FETCHED FROM DATABASE !
 11067 ANUJ UK
 11057 ADITYA  PUNE
+
+INSERT :
+RECORD FETCHED FROM DATABASE !
+11067 ANUJ UK
+11057 ADITYA  PUNE
+11080 Banergi VimanNagar
  */
