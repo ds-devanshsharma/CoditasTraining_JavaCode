b88dee4d2db227ad52caf59cb0613004c480e206
ds-devanshsharma <94562043+ds-devanshsharma@users.noreply.github.com>
2022-09-15 17:44:09 +0530

    AdminUserApplication done

---
+++ src/Servlet-Code/AdminUserApplication/src/main/java/UserBean.java
@@ -0,0 +1,41 @@
+public class UserBean {
+    private int id;
+    private String name;
+    private String city;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserBean() {
+    }
+}
---
+++ src/Servlet-Code/AdminUserApplication/src/main/java/UserDisplayServlet.java
@@ -0,0 +1,45 @@
+import JDBCConnection.ConnectionImplementation;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+@WebServlet("/userDisplay")
+public class UserDisplayServlet extends HttpServlet {
+    PreparedStatement preparedStatement;
+    ResultSet resultSet;
+    UserBean userBean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        try {
+            preparedStatement = ConnectionImplementation.getJDBCConnection()
+                    .prepareStatement("SELECT * FROM USER ");
+            resultSet = preparedStatement.executeQuery();
+            out.println("<center><div  style='border:2px solid black; height: 400px; width:50%' >" +
+                    "<h1>User Data</h1><hr><br>" +
+                    "<table border='1' > " +
+                    "<tr><th>ID </th><th>UserName</th><th>City</th></tr>");
+            while(resultSet.next()){
+                out.println("<tr><td>"+resultSet.getInt(1)+"</td>"+
+                        "<td>" +resultSet.getString(2)+"</td>"+
+                        "<td>"+resultSet.getString(3)+"</td></tr>");
+            }
+            out.print("</table>");
+            out.println("<form action ='RegisterForm.html'>" +
+                        "<Button type= 'submit'>Register</Button></form>&nbsp;");
+            out.println("<a href='DeleteForm.html'><button type='button'>Delete</button></a>&nbsp;");
+            out.println("<a href='logout'><button type='button'>logout</button></a></div></center>");
+
+        }catch(Exception e ){
+
+        }
+    }
+}
